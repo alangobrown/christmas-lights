aac2e929199d261f3af35259b642b2a2bdd4e306
Alan Brown <alan@mailmob.co.uk>
2014-12-20 21:58:58 +0000

    initial commit

---
+++ app/views.py
@@ -0,0 +1,33 @@
+from flask import render_template
+import RPi.GPIO as GPIO
+
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+
+
+@app.route('/on')
+def on():
+
+
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(7,GPIO.OUT)
+	GPIO.output(7,False)
+	pin7 = str(GPIO.input(7))
+	return 'Switched the lights on.  Current status is ' + pin7
+
+
+@app.route('/off')
+def off():
+
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(7,GPIO.OUT)
+	GPIO.output(7,True)
+	pin7 = str(GPIO.input(7))
+	return 'Switched the lights off.  Current status is ' + pin7
