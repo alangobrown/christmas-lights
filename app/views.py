6dec2c8dcf57dfcf31e9ddbbfc6736b93a02946f
Alan Brown <alan@mailmob.co.uk>
2014-12-20 23:10:07 +0000

    added bootstrap

--- app/views.py
+++ app/views.py
@@ -17,8 +17,12 @@ def on():
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(7,GPIO.OUT)
 	GPIO.output(7,False)
-	pin7 = str(GPIO.input(7))
-	return 'Switched the lights on.  Current status is ' + pin7
+	pin7 = str(not GPIO.input(7))
+
+	return render_template('index.html',
+                           title='On',
+                           status=pin7)
+
 
 
 @app.route('/off')
@@ -27,5 +31,7 @@ def off():
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(7,GPIO.OUT)
 	GPIO.output(7,True)
-	pin7 = str(GPIO.input(7))
-	return 'Switched the lights off.  Current status is ' + pin7
+	pin7 = str(not GPIO.input(7))
+	return render_template('index.html',
+                           title='Off',
+                           status=pin7)
