066dbd53dbedea50dfaa97e2d54bb74ee042ef10
Alan Brown <alan@mailmob.co.uk>
2016-11-19 22:34:17 +0000

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,8 +3,11 @@ from flask import render_template
 
 from app import app
 
+import socket
+print(socket.gethostname())
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html',
-                           title='Home')
+                           title='Home', host=socket.gethostname())
